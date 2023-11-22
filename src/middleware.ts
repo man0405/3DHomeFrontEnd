fe84d01548810b6b162ec160d439bc02229a9114
man0405 <vanman160504@gmail.com>
2023-11-22 15:43:14 +0700

    update middlerware v2

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,33 @@ import Cookies from "cookies";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest, response: NextResponse) {
+export async function middleware(request: NextRequest) {
 	const url = request.nextUrl.clone();
-	url.pathname = "/auth/signin";
+
 	let cookie = request.cookies.get("uss");
+	if (url.pathname === "/") {
+		if (cookie) {
+			const response = await fetch(
+				process.env.NEXT_PUBLIC_API_URL + "/api/v1/test/verify",
+				{
+					method: "GET",
+					headers: {
+						Authorization: "Bearer " + cookie.value,
+					},
+				}
+			);
+			if (response.status === 200) {
+				url.pathname = "/home";
+				return NextResponse.redirect(url);
+			}
+			if (response.status === 403) {
+				return NextResponse.next();
+			}
+		}
+		return;
+	}
+
+	url.pathname = "/auth/signin";
 	if (!cookie) {
 		return NextResponse.redirect(url);
 	} else {
@@ -29,5 +52,5 @@ export async function middleware(request: NextRequest, response: NextResponse) {
 }
 
 export const config = {
-	matcher: ["/home/:path*", "/setting/:path*", "/profile"],
+	matcher: ["/", "/home/:path*", "/setting/:path*", "/profile"],
 };
