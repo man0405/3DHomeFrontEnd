18a2eb8198de4f9b362bda71c86cfb07910e723f
man0405 <vanman160504@gmail.com>
2023-11-22 14:38:41 +0700

    update middlerware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,30 @@
+import Cookies from "cookies";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest, response: NextResponse) {
+export async function middleware(request: NextRequest, response: NextResponse) {
 	const url = request.nextUrl.clone();
 	url.pathname = "/auth/signin";
 	let cookie = request.cookies.get("uss");
 	if (!cookie) {
 		return NextResponse.redirect(url);
+	} else {
+		console.log(cookie.value);
+		const response = await fetch(
+			process.env.NEXT_PUBLIC_API_URL + "/api/v1/test/verify",
+			{
+				method: "GET",
+				headers: {
+					Authorization: "Bearer " + cookie.value,
+				},
+			}
+		);
+		if (response.status === 200) {
+			return NextResponse.next();
+		}
+		if (response.status === 403) {
+			return NextResponse.redirect(url);
+		}
 	}
 }
 
