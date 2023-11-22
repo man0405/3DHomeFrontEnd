a9c4a2ca111505a6378c6ecabd40262ff65483c3
man0405 <vanman160504@gmail.com>
2023-11-22 13:22:03 +0700

    simple middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {}
+export function middleware(request: NextRequest, response: NextResponse) {
+	const url = request.nextUrl.clone();
+	url.pathname = "/auth/signin";
+	let cookie = request.cookies.get("uss");
+	if (!cookie) {
+		return NextResponse.redirect(url);
+	}
+}
+
+export const config = {
+	matcher: ["/home/:path*", "/setting/:path*", "/profile"],
+};
